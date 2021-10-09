e1466bdb756ade81fc48713d6dae36e82b2f5023
Anthony Metzidis <anthony.metzidis@gmail.com>
2021-10-09 16:03:13 -0700

    google keep export parser

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+const (
+	stateSkip = iota
+	stateStart
+	stateHeading
+	stateReading
+)
+
+var (
+	patternHeading = regexp.MustCompile("class.*c5")
+	patternStart   = regexp.MustCompile("<body class=\"c12\"")
+)
+
+func scanFile() error {
+	var (
+		docBuf   []string = make([]string, 0, 5)
+		curState int      = stateSkip
+		fileNum  int      = 0
+		fileName string
+	)
+
+	//for m, i, err := 0, 0, error(nil); err != io.EOF; i++ {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if patternStart.Match([]byte(line)) {
+			curState = stateStart
+		} else if patternHeading.Match([]byte(line)) {
+			curState = stateHeading
+		}
+
+		switch curState {
+		case stateSkip:
+		case stateStart:
+			fmt.Printf("start: %s\n", line)
+		case stateHeading:
+			if len(docBuf) > 0 {
+				// if buf is not empty, write out the file
+				fileName = fmt.Sprintf("output/%d.html", fileNum)
+				if err := writeFile(docBuf, fileName); err != nil {
+					panic(err)
+				}
+				fileNum++
+			}
+			// init buf
+			docBuf = make([]string, 0, 5)
+			// start appending
+			docBuf = append(docBuf, line)
+			fmt.Printf("heading: %s\n", line)
+			// next state
+			curState = stateReading
+		case stateReading:
+			docBuf = append(docBuf, line)
+		}
+
+	}
+	return nil
+}
+
+func writeFile(docBuf []string, fileName string) error {
+	f, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	docBuf = append([]string{"<html>\n<body>\n"}, docBuf...)
+	docBuf = append(docBuf, "</body>\n</html>\n")
+
+	defer f.Close()
+	if _, err2 := f.WriteString(strings.Join(docBuf, "\n")); err2 != nil {
+		panic(err2)
+	}
+	return nil
+}
+
+func main() {
+	scanFile()
+}
